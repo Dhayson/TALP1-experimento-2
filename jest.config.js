a58e384a606aa491d309d19a7522cb52fb99a168
Diego <djaar@cin.ufpe.br>
2026-04-20 03:41:59 -0300

    Phase 2: fix unit tests configuration

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,18 @@
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/tests/unit'],
-  testMatch: ['**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  collectCoverageFrom: [
-    'server/src/**/*.ts',
-    '!server/src/**/*.d.ts'
-  ],
-  coverageDirectory: 'coverage',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  rootDir: ".",
+  testRegex: "/tests/unit/.*\\.test\\.ts$",
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.test.json'
+    }]
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverageFrom: ["server/src/**/*.ts", "!server/src/**/*.d.ts"],
+  coverageDirectory: "coverage",
   verbose: true,
   moduleNameMapper: {
-    '^uuid$': '<rootDir>/tests/__mocks__/uuid.js'
+    "^uuid$": "./tests/__mocks__/uuid.js",
   },
 };
